978afd5d564049ffe47eca21547567b3d0b507fb
John Helder <helderjohn27@gmail.com>
2025-12-30 22:45:36 -0300

    chore: initial project structure

---
+++ smartexpense-backend/src/main/java/com/smartexpense/SmartexpenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartexpenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartexpenseBackendApplication.class, args);
+	}
+
+}
